2f048580b347d198cabf7a5e8eb77e50422b0b02
Bachelor Bratchikov <bratchkov1999@gmail.com>
2022-12-15 01:07:06 +0700

    Reload New lab

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Model;
+
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Person person = new Person(string.Empty, "Ivanov", 20);
+            }
+            catch (ArgumentException exeption)
+            {
+                if (exeption.GetType() == typeof(ArgumentNullException)
+                    || exeption.GetType() == typeof(ArgumentException))
+                {
+                    Console.WriteLine(exeption.Message);
+                }
+            }
+
+            Person partner = new Person("Anastasia", "Kuznetsova", 19);
+            /*
+            person.Partner = partner;
+
+            for (int i = 0; i < 20; i++)
+            {
+                person.CelebrateHappyBerthday();
+            }
+
+            Console.WriteLine(person.GetInfo());*/
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Model/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Model
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Person
+    {
+
+        public string Name 
+        {
+            get
+            {
+                return _name;
+            }
+            private set
+            {
+                _name = value;
+            }
+                
+        }
+
+        internal string _name;
+
+        private string _surname;
+
+        private int _age;
+
+        public Person Partner;
+
+        /// <summary>
+        /// Создается эеземпляр класса <see cref="Person"/>.
+        /// </summary>
+        /// <param name="name">Имя человека.</param>
+        /// <param name="surname">Фамилия.</param>
+        /// <param name="age">Возраст.</param>
+        /// <exception cref="ArgumentException"></exception>
+        public Person(string name, string surname, int age)
+        {
+            if (name == null)
+            {
+                throw new ArgumentException("Name should not be null!");
+            }
+
+            if (name == string.Empty)
+            {
+                throw new ArgumentException("Name should not be empty!");
+            }
+
+            _name = name;
+            _surname = surname;
+            _age = age;
+        }
+
+        public string GetInfo()
+        {
+            return $"Person name: {_name}, surname {_surname}, age is {_age}. \n" +
+                $"Person Partner {Partner?.GetInfo()}";
+        }
+
+        public void CelebrateHappyBerthday()
+        {
+            _age++;
+        }
+        
+    }
+}
